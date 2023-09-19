de7599f34f0384916ddc68d0bca36954b007e581
Nonobari <noe.backert@gmail.com>
2023-09-19 12:22:16 +0100

    version quasi finale, avant cache

--- lib/menu.dart
+++ lib/menu.dart
@@ -3,6 +3,7 @@ import 'package:comif_app/login.dart';
 import 'package:comif_app/token.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert' as convert;
 import 'token.dart';
 
